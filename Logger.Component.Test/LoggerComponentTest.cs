0609b29db1c45824e805c37e60b4db4670ac416f
Alexander Borovskikh <borislav_god@mail.ru>
2018-05-02 20:33:06 +0500

    начало написания тестов

---
+++ Logger.Component.Test/LoggerComponentTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using PdfSeparator.Model.Interface;
+
+namespace Logger.Component.Test
+{
+    [TestFixture]
+    public class LoggerComponentTest
+    {
+        public void Logging_RecordTest()
+        {
+            ILogger logger = new PdfSeparator.Model.Components.Logger();
+        }
+    }
+}
---
+++ PdfSeparatorTest/PdfComponentTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace PdfSeparatorTest
+{
+    [TestFixture]
+    public class PdfComponentTest
+    {
+        [Test]
+        public void Open_OpenedFile()
+        {
+
+        }
+    }
+}
