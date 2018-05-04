1279d01cb9153930a3265c68fadcf8013b855223
Alexander Borovskikh <borislav_god@mail.ru>
2018-05-04 16:28:10 +0500

    Окончание рабочей смены

--- PdfSeparatorTest/PdfComponentTest.cs
+++ PdfSeparatorTest/PdfComponentTest.cs
@@ -20,6 +20,9 @@ namespace PdfSeparatorTest
             _pdf = new PdfComponet();
         }
 
+        /// <summary>
+        /// Проверка открытия файла в компонете
+        /// </summary>
         [Test]
         public void PdfComponent_Open()
         {
@@ -30,22 +33,74 @@ namespace PdfSeparatorTest
             Assert.IsTrue(_pdf.IsOpen);
         }
 
+        /// <summary>
+        /// Проверка на исключение при повторном открытии файла без закрытия предыдущего
+        /// </summary>
+        [Test]
+        public void PdfComponent_OpenSecondary()
+        {
+            const string PDF_FILE = @"C:\Users\Alexander\Desktop\ИУС\НАЦИОНАЛЬНЫЕ СТАНДАРТЫ.pdf";
+
+            // Открываем файл в компоненте
+            _pdf.Open(PDF_FILE);
+
+            // Проверяем на исключение при попытке открыть повторно или другой файл в компоненте
+            Assert.Throws<Exception>(() => _pdf.Open(PDF_FILE));
+        }
+
+        /// <summary>
+        /// Проверка на закрытие файла
+        /// </summary>
         [Test]
         public void PdfComponent_Close()
         {
             const string PDF_FILE = @"C:\Users\Alexander\Desktop\ИУС\НАЦИОНАЛЬНЫЕ СТАНДАРТЫ.pdf";
 
+            // Открываем файл в компоненте
             _pdf.Open(PDF_FILE);
 
+            // Закрываем файл в компоненте
             _pdf.Close();
 
             Assert.IsFalse(_pdf.IsOpen);
         }
 
+        /// <summary>
+        /// Проверка на повторное закрытие файла при его отсутствии в компоненте
+        /// </summary>
+        [Test]
+        public void PdfComponent_CloseSecondary()
+        {
+            // Проверяем на исключение при попытке закрыть повторно файл в компоненте
+            Assert.Throws<Exception>(() => _pdf.Close());
+        }
+
+        /// <summary>
+        /// Проверка получения коллекции глав по форматам
+        /// </summary>
         [Test]
         public void PdfComponent_GetCharpters()
         {
+            const string PDF_FILE = @"C:\Users\Alexander\Desktop\ИУС\НАЦИОНАЛЬНЫЕ СТАНДАРТЫ.pdf";
+            
+            // Открываем файл в компоненте
+            _pdf.Open(PDF_FILE);
 
+            Assert.IsNotEmpty(_pdf.GetChapters);
+        }
+
+        /// <summary>
+        /// Проверка получения количества страниц в документе
+        /// </summary>
+        [Test]
+        public void PdfComponent_Count()
+        {
+            const string PDF_FILE = @"C:\Users\Alexander\Desktop\ИУС\НАЦИОНАЛЬНЫЕ СТАНДАРТЫ.pdf";
+
+            // Открываем файл в компоненте
+            _pdf.Open(PDF_FILE);
+
+            Assert.AreNotEqual(0, _pdf.Count);
         }
     }
 }
