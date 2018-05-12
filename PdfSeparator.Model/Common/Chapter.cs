1ae8d4bfae18d47aacfd96213cb9979104db9e1c
Alexander Borovskikh <borislav_god@mail.ru>
2018-05-12 15:32:06 +0500

    Add comments

--- PdfSeparator.Model/Common/Chapter.cs
+++ PdfSeparator.Model/Common/Chapter.cs
@@ -7,11 +7,34 @@ namespace PdfSeparator.Model.Common
     // ToDo: Добавить комментарии
     public class Chapter : IChapter
     {
+        /// <summary>
+        /// Получение или установка наименование формата главы
+        /// </summary>
         public string Format { get; set; }
+
+        /// <summary>
+        /// Получение номера страницы начала главы
+        /// </summary>
         public int Start => Pages.First().Position;
+
+        /// <summary>
+        /// Получение номера страницы окончания главы
+        /// </summary>
         public int End => Pages.Last().Position;
+
+        /// <summary>
+        /// Получение общего количества страниц в данной главе
+        /// </summary>
         public int Count => Pages.Count;
+
+        /// <summary>
+        /// Получение или установка коллекции страниц входящие в главу
+        /// </summary>
         public List<IPage> Pages { get; set; }
+
+        /// <summary>
+        /// Получение или установка необходимости добавления пустой страницы в конце главы
+        /// </summary>
         public bool AddBlankPageToEnd { get; set; }
 
         public Chapter(string format)
--- PdfSeparator.Model/Interface/IChapter.cs
+++ PdfSeparator.Model/Interface/IChapter.cs
@@ -2,14 +2,36 @@
 
 namespace PdfSeparator.Model.Interface
 {
-    // ToDo: Добавить комментарии
     public interface IChapter
     {
+        /// <summary>
+        /// Получение или установка наименование формата главы
+        /// </summary>
         string Format { get; set; }
+
+        /// <summary>
+        /// Получение номера страницы начала главы
+        /// </summary>
         int Start { get; }
+
+        /// <summary>
+        /// Получение номера страницы окончания главы
+        /// </summary>
         int End { get; }
+
+        /// <summary>
+        /// Получение общего количества страниц в данной главе
+        /// </summary>
         int Count { get; }
+
+        /// <summary>
+        /// Получение или установка коллекции страниц входящие в главу
+        /// </summary>
         List<IPage> Pages { get; set; }
+
+        /// <summary>
+        /// Получение или установка необходимости добавления пустой страницы в конце главы
+        /// </summary>
         bool AddBlankPageToEnd { get; set; }
     }
 }
